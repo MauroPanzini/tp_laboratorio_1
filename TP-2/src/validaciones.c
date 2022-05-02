1c4f34a84fbdfb8407c1b6bf55173b2f1614ac35
Mauro Panzini <mauropanzini18@gmail.com>
2022-05-01 21:31:16 -0300

    TP-2 empezado

---
+++ TP-2/src/validaciones.c
@@ -0,0 +1,26 @@
+/*
+ * validaciones.c
+ *
+ *  Created on: 26 abr. 2022
+ *      Author: mauropanzini
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int esNumero(float num){
+	int retorno = -1;
+
+
+	return retorno;
+}
+int pedirDatos(){
+
+	char name[51];
+	char lastname[51];
+	float price;
+	char flycode[10];
+	int typePassanger, statusFlight, isEmpty, id[2000], retorno;
+	return retorno;
+}
---
+++ TP-2/src/validaciones.h
@@ -0,0 +1,13 @@
+/*
+ * validaciones.h
+ *
+ *  Created on: 26 abr. 2022
+ *      Author: mauropanzini
+ */
+
+#ifndef VALIDACIONES_H_
+#define VALIDACIONES_H_
+
+
+
+#endif /* VALIDACIONES_H_ */
